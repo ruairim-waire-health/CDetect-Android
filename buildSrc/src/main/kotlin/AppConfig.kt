79f7343d9a8c5fa33029c84deffc04dcde6a5603
Ruairi M <ruairim@waire.health>
2023-03-29 10:17:54 +0100

    added App hilt entry point

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,10 +1,13 @@
 object AppConfig {
-    const val compileSdkVersion = 32
+    const val app_namespace = "com.waire.cdetect.android"
+    const val data_namespace = "com.waire.cdetect.data"
+    const val domain_namespace = "com.waire.cdetect.domain"
+    const val compileSdkVersion = 33
     const val minSdkVersion = 26
-    const val targetSdkVersion = 32
+    const val targetSdkVersion = 33
     const val versionCode = 1
     const val versionName = "1.0"
     const val testInstrumentationRunner = "i.am.bored.di.HiltTestRunner"
-    const val applicationId = "id.techtask.whitelabel"
+    const val applicationId = "com.waire.cdetect.android"
     const val consumerProguardFiles = "consumer-rules.pro"
 }
