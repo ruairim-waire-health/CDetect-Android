dd6d78996585984ce8614ff709d229f926742a10
Ruairi <ruairi.mcguigan@spirit-health.com>
2023-04-10 16:44:12 +0100

    bumped target api to 28

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -4,10 +4,9 @@ object AppConfig {
     const val domain_namespace = "com.waire.cdetect.domain"
     const val compileSdkVersion = 33
     const val minSdkVersion = 26
-    const val targetSdkVersion = 33
+    const val targetSdkVersion = 28
     const val versionCode = 1
     const val versionName = "1.0"
-    const val testInstrumentationRunner = "i.am.bored.di.HiltTestRunner"
     const val applicationId = "com.waire.cdetect.android"
     const val consumerProguardFiles = "consumer-rules.pro"
 }
